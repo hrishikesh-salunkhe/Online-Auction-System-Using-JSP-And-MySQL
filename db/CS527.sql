b09389f6ef8410bd096bced8f89dfc51d35e739c
Hrishikesh Salunkhe <hsalunkhe1998@gmail.com>
2023-04-22 18:15:43 -0400

    Added the db script

--- db/CS527.sql
+++ db/CS527.sql
@@ -33,19 +33,43 @@ create table modifiedBy(custRepId VARCHAR(10), userId VARCHAR(10), datetime VARC
 select * from modifiedBy;
 
 -- QUESTION:
-create table question(questionId int NOT NULL AUTO_INCREMENT PRIMARY KEY, questionDetails VARCHAR(100), solutionDetails VARCHAR(100),
+create table question(questionId INT NOT NULL AUTO_INCREMENT PRIMARY KEY, questionDetails VARCHAR(100), solutionDetails VARCHAR(100),
 						custRepId VARCHAR(10), userId VARCHAR(10),
                         FOREIGN KEY (custRepId) REFERENCES custRep(custRepId),
                         FOREIGN KEY (userId) REFERENCES user(userId));
 
 select * from question;
 
+-- AUTO BID:
+create table autoBid(autoBidId INT NOT NULL AUTO_INCREMENT PRIMARY KEY, bidLimit FLOAT,
+					itemId INT NOT NULL, isLimitCrossed CHAR(1),
+					FOREIGN KEY (itemId) REFERENCES AuctionItem(itemId));
+
+select * from autoBid;
+
+-- BID:
+create table bid(bidId INT NOT NULL AUTO_INCREMENT PRIMARY KEY, userId VARCHAR(10), dateTime VARCHAR(50),
+				amount FLOAT, itemId INT NOT NULL, autoBidId INT, FOREIGN KEY (itemId) REFERENCES auctionItem(itemId), 
+				FOREIGN KEY (userId) REFERENCES user(userId), FOREIGN KEY (autoBidId) REFERENCES AutoBid(autoBidId));
+
+select * from bid;
+
+-- RESERVES:
+create table reserves(bidId INT, itemId INT, PRIMARY KEY(bidId, itemId),
+						FOREIGN KEY (bidId) REFERENCES bid(bidId), FOREIGN KEY (itemId) REFERENCES auctionItem(itemId));
+
+select * from reserves;
+
 -- AUCTION ITEM:
-create table auctionItem(itemId int NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(40),
-						subcategory VARCHAR(30), initialPrice FLOAT,
-						minPrice FLOAT, closingDateTime DATETIME, isValid CHAR(1),
-						features VARCHAR(100), delisterId VARCHAR(10), incrementAmount FLOAT, currentBid FLOAT,
-						sellerId VARCHAR(10) NOT NULL, buyerId VARCHAR(10),
-						FOREIGN KEY (sellerId) REFERENCES user(userId),
-						FOREIGN KEY (buyerId) REFERENCES bid(userId),
-						FOREIGN KEY (delisterId) REFERENCES CustomerRep(custRepId) )
+-- colorType: (Acrylic, Oil, Watercolor, Pastel, Gouache, Encaustic, Fresco, Spray Paint, Digital)
+-- paintingStyle: (Realism, Photorealism, Expressionism, Impressionism, Abstract, Surrealism, Pop Art)
+-- subcategory: (History Painting, Portrait Art, Genre Painting, Landscape Painting, Still Life Painting)
+
+create table auctionItem(itemId INT NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(40),
+						subcategory VARCHAR(40), length INT, breadth INT, colorType VARCHAR(20), 
+                        paintingStyle VARCHAR(20), description VARCHAR(100), artist VARCHAR(50),
+                        initialPrice FLOAT, minPrice FLOAT, closingDateTime VARCHAR(50),
+						incrementAmount FLOAT, currentBid FLOAT, sellerId VARCHAR(10) NOT NULL, buyerId VARCHAR(10),
+						FOREIGN KEY (sellerId) REFERENCES user(userId));
+                        
+select * from auctionItem;
